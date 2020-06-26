8362bcf70276598ced83ffb6a86e4711dfbefab7
Victoriano Giralt <victoriano@uma.es>
2020-06-26 02:00:51 +0200

    Traducción al castellano

--- forms.py
+++ forms.py
@@ -11,7 +11,7 @@ TECHS=[(t.id,t.name) for t in Technician.objects.all()]
 
 class loadBatch(forms.Form):
     batchid = forms.CharField(max_length=32,required=False,
-                    label=_('Batch identifier (leave blank to geneate)'))
+                    label=_('Batch identifier (leave blank to generate)'))
     samples = forms.FileField(label=_('Samples file'),required=False)
     techid = forms.IntegerField(label=_('Technician'),
                                 widget=forms.Select(choices=TECHS))
