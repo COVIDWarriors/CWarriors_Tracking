4bd85b781a7ff9e56cc38e3133b8e588b611c085
Victoriano Giralt <victoriano@uma.es>
2020-06-30 10:59:20 +0200

    deployability fix

--- forms.py
+++ forms.py
@@ -7,7 +7,10 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 from .models import Technician
 
-TECHS=[(t.id,t.name) for t in Technician.objects.all()]
+# I know this is crap, I'll improve on a future iteration
+#TECHS=[(t.id,t.name) for t in Technician.objects.all()]
+# Uncomment previous line once database has been created, and remove this and next line
+TECHS = [(0,'See forms.py to configure')]
 
 class loadBatch(forms.Form):
     batchid = forms.CharField(max_length=32,required=False,
