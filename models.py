8e835559432fff4572467e56b130362e7e7c522f
Victoriano Giralt <victoriano@uma.es>
2020-12-02 23:45:49 +0100

    Recoreved PEP8 and Django 2.2 mods

--- models.py
+++ models.py
@@ -5,40 +5,40 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
-import time,datetime,uuid
+import time, datetime, uuid
 
 # Create your models here.
 class Robot(models.Model):
-    TYPES = [('A','A'),('B','B'),('C','C')]
-    STATES = [('I',_('Iddle')),('F',_('Finished')),('E',_('Empty')),
-              ('P',_('Processing')),('D',_('Not connected'))]
+    TYPES = [('A', 'A'), ('B', 'B'), ('C', 'C')]
+    STATES = [('I', _('Iddle')), ('F', _('Finished')), ('E', _('Empty')),
+              ('P', _('Processing')), ('D', _('Not connected'))]
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=True)
-    station = models.CharField(max_length=1,verbose_name=_('Station'),
-                               db_index=True,choices=TYPES)
+    station = models.CharField(max_length=1, verbose_name=_('Station'),
+                               db_index=True, choices=TYPES)
     order = models.IntegerField(verbose_name=_('Station order'),
-                                db_index=True,default=1)
+                                db_index=True, default=1)
     ip = models.GenericIPAddressField(verbose_name=_('IP address'),
                                       max_length=255, unique=True)
     connected = models.BooleanField(verbose_name=_('Can communicate with server'),
-                                    default=True,db_index=True, editable=True)
-    state = models.CharField(max_length=1,verbose_name=_('State'),default='D',
-                             db_index=True,choices=STATES)
+                                    default=True, db_index=True, editable=True)
+    state = models.CharField(max_length=1, verbose_name=_('State'), default='D',
+                             db_index=True, choices=STATES)
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
 
     class Meta:
-        ordering = ['station','order','-modifiedOn']
+        ordering = ['station', 'order', '-modifiedOn']
 
 
     def __str__(self):
-        return '{0}{1} {2}'.format(self.station,self.order,self.ip)
+        return '{0}{1} {2}'.format(self.station, self.order, self.ip)
 
 
     def save(self, *args, **kwargs):
@@ -67,17 +67,17 @@ class Technician(models.Model):
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=False)
-    name = models.CharField(max_length=100,verbose_name=_('Name'),db_index=True)
+    name = models.CharField(max_length=100, verbose_name=_('Name'), db_index=True)
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
 
     class Meta:
-        ordering = ['name','-modifiedOn']
+        ordering = ['name', '-modifiedOn']
 
 
     def __str__(self):
@@ -114,10 +114,10 @@ class Procedure(models.Model):
 
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
 
     class Meta:
@@ -134,32 +134,33 @@ class Batch(models.Model):
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=False)
-    technician = models.ForeignKey(Technician)
-    procedure = models.ForeignKey(Procedure, null=True)
+    technician = models.ForeignKey(Technician, on_delete=models.DO_NOTHING)
+    procedure = models.ForeignKey(Procedure, null=True,
+                                  on_delete=models.DO_NOTHING)
     preloaded = models.BooleanField(verbose_name=_('Pre-loaded batch'),
                                     default=True)
     started = models.DateTimeField(_('Processing started on'),
-                                   db_index=True,null=True,blank=True)
+                                   db_index=True, null=True, blank=True)
     finished = models.DateTimeField(_('Processing completed on'),
-                                    db_index=True,null=True,blank=True)
+                                    db_index=True, null=True, blank=True)
 
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
  
     class Meta:
         verbose_name_plural = _('Batches')
-        ordering = ['identifier','technician','-modifiedOn']
+        ordering = ['identifier', 'technician', '-modifiedOn']
 
 
     def __str__(self):
         return _('{} ({}): {} -> {} by {}').format(self.identifier,
                                                    len(self.sample_set.all()),
-                                                   self.started,self.finished,
+                                                   self.started, self.finished,
                                                    self.technician)
 
 
@@ -182,15 +183,16 @@ class Batch(models.Model):
 
 
 class Sample(models.Model):
-    RESULTS = [('N',_('No')),('Y',_('Yes')),('U',_('Unclear'))]
-    LEVELS = [('L',_('Low')),('M',_('Medium')),('H',_('High'))]
+    RESULTS = [('N', _('No')), ('Y', _('Yes')), ('U', _('Unclear'))]
+    LEVELS = [('L', _('Low')), ('M', _('Medium')), ('H', _('High'))]
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=False)
-    code = models.CharField(verbose_name=_('Sample code'), max_length=100, db_index=True)
-    batch = models.ForeignKey(Batch)
+    code = models.CharField(verbose_name=_('Sample code'), max_length=100,
+                            db_index=True)
+    batch = models.ForeignKey(Batch, on_delete=models.DO_NOTHING)
     finished = models.BooleanField(verbose_name=_('Processig completed'),
-                                   db_index=True,default=False)
+                                   db_index=True, default=False)
     result = models.CharField(verbose_name=_('Result'), max_length=1, db_index=True,
                               choices=RESULTS, blank=True, null=True, default='')
     igg = models.CharField(verbose_name=_('IgG'), max_length=1, db_index=True,
@@ -199,18 +201,18 @@ class Sample(models.Model):
                            choices=LEVELS, blank=True, null=True, default='')
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
 
     class Meta:
-        ordering = ['batch','-modifiedOn']
+        ordering = ['batch', '-modifiedOn']
 
 
     def __str__(self):
-        return _('{0} en {1}').format(self.code,self.batch)
+        return _('{0} en {1}').format(self.code, self.batch)
 
 
     def save(self, *args, **kwargs):
@@ -226,17 +228,17 @@ class Tray(models.Model):
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=False)
-    batch = models.ForeignKey(Batch)
+    batch = models.ForeignKey(Batch, on_delete=models.DO_NOTHING)
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
 
     class Meta:
-        ordering = ['batch','-modifiedOn']
+        ordering = ['batch', '-modifiedOn']
 
 
     def save(self, *args, **kwargs):
@@ -257,32 +259,33 @@ class Rack(models.Model):
     DEEPWELL = 5
     EP = 6
     PCR = 7
-    TYPE = [(4,'4x6'),(5,_('Deepwell')),
-            (6,_('Extraction Plate')),(7,_('PCR Plate'))]
-    POSITION = [(1,'1'),(2,'2'),(3,'3'),(4,'4'),(5,'5'),(6,'6')]
-    ROWCOLS = {1:('D',1),2:('D',2),3:('D',4),4:('D',6),5:('H',12),
-               6:('H',12),7:('H',12)} 
+    TYPE = [(4, '4x6'), (5, _('Deepwell')),
+            (6, _('Extraction Plate')), (7, _('PCR Plate'))]
+    POSITION = [(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6')]
+    ROWCOLS = {1:('D', 1), 2:('D', 2), 3:('D', 4), 4:('D', 6), 5:('H', 12),
+               6:('H', 12), 7:('H', 12)} 
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=False)
     racktype = models.IntegerField(verbose_name=_('Rack type'),
-                                   choices=TYPE,db_index=True)
-    position = models.IntegerField(choices=POSITION,db_index=True,default=0)
-    robot = models.ForeignKey(Robot,null=True,blank=True)
+                                   choices=TYPE, db_index=True)
+    position = models.IntegerField(choices=POSITION, db_index=True, default=0)
+    robot = models.ForeignKey(Robot, null=True, blank=True,
+                              on_delete=models.DO_NOTHING)
     finished = models.BooleanField(verbose_name=_('Processing completed'),
-                                   default=False,db_index=True)
+                                   default=False, db_index=True)
     passed = models.CharField(verbose_name=_('Steps'), max_length=3,
                               blank=True, default='', db_index=True)
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
  
     def listCols(self):
-        return [(x,'%d' % x) for x in range(1,self.ROWCOLS[self.racktype][1]+1)]
+        return [(x, '%d' % x) for x in range(1, self.ROWCOLS[self.racktype][1]+1)]
 
 
     def listRows(self):
@@ -319,15 +322,15 @@ class Rack(models.Model):
             return _('{0} with {1} samples in {2} tray {3}').format(
                                              self.get_racktype_display(),
                                              self.numSamples(),
-                                             self.robot,self.position)
+                                             self.robot, self.position)
         else:
             return _('{0} with {1} samples {2} [{3}]').format(
                                              self.get_racktype_display(),
                                              self.numSamples(),
-                                             self.identifier,self.passed)
+                                             self.identifier, self.passed)
 
     def shortIdent(self):
-        return '{0}...{1}'.format(self.identifier[:9],self.identifier[-9:])
+        return '{0}...{1}'.format(self.identifier[:9], self.identifier[-9:])
 
 
     def clean(self):
@@ -375,29 +378,29 @@ class Rack(models.Model):
 class Tube(models.Model):
     FILAS = [('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'),
              ('E', 'E'), ('F', 'F'), ('G', 'G'), ('H', 'H')]
-    COLS = [(x,'%d' % x) for x in range(1,13)]
+    COLS = [(x, '%d' % x) for x in range(1, 13)]
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=False)
-    row = models.CharField(max_length=1,choices=FILAS,db_index=True)
-    col = models.IntegerField(choices=COLS,db_index=True)
-    history = models.CharField(max_length=250,default='',
-                               blank=True,editable=False)
-    rack = models.ForeignKey(Rack)
-    sample = models.ForeignKey(Sample)
+    row = models.CharField(max_length=1, choices=FILAS, db_index=True)
+    col = models.IntegerField(choices=COLS, db_index=True)
+    history = models.CharField(max_length=250, default='',
+                               blank=True, editable=False)
+    rack = models.ForeignKey(Rack, on_delete=models.DO_NOTHING)
+    sample = models.ForeignKey(Sample, on_delete=models.DO_NOTHING)
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
     class Meta:
         ordering = ['createdOn']
 
 
     def __str__(self):
-        return '{0} {1}:{2} {3}'.format(self.rack,self.row,self.col,
+        return '{0} {1}:{2} {3}'.format(self.rack, self.row, self.col,
                                         self.sample.code)
 
 
@@ -415,7 +418,7 @@ class Tube(models.Model):
             self.identifier = uuid.uuid4().hex
         if not self.history == '':
             self.history += ' '
-        current = '{0}:{1},{2}'.format(self.rack.identifier,self.row,self.col)
+        current = '{0}:{1}, {2}'.format(self.rack.identifier, self.row, self.col)
         if current not in self.history:
             self.history += current
 
@@ -426,20 +429,20 @@ class Tube(models.Model):
 
 
 class Log(models.Model):
-    DIR = [('I',_('ENTERS')),('O',_('EXITS'))]
+    DIR = [('I', _('ENTERS')), ('O', _('EXITS'))]
     identifier = models.CharField(verbose_name=_('identifier'),
                                   max_length=32, blank=True, null=True,
                                   db_index=True, editable=False)
     what = models.CharField(verbose_name=_('In / Out'),
-                            max_length=1,choices=DIR,db_index=True)
-    robot = models.ForeignKey(Robot)
-    rack = models.ForeignKey(Rack)
+                            max_length=1, choices=DIR, db_index=True)
+    robot = models.ForeignKey(Robot, on_delete=models.DO_NOTHING)
+    rack = models.ForeignKey(Rack, on_delete=models.DO_NOTHING)
 
     # Control information
-    createdOn = models.DateTimeField(_('Created on'),auto_now_add=True,
-                                     db_index=True,editable=False)
-    modifiedOn = models.DateTimeField(_('Modified on'),auto_now=True,
-                                      db_index=True,editable=False)
+    createdOn = models.DateTimeField(_('Created on'), auto_now_add=True,
+                                     db_index=True, editable=False)
+    modifiedOn = models.DateTimeField(_('Modified on'), auto_now=True,
+                                      db_index=True, editable=False)
 
  
     class Meta:
@@ -448,7 +451,7 @@ class Log(models.Model):
 
     def __str__(self):
         return '{} {} {} {}'.format(self.createdOn,
-                                    self.rack,self.what,self.robot)
+                                    self.rack, self.what, self.robot)
 
 
     def save(self, *args, **kwargs):
